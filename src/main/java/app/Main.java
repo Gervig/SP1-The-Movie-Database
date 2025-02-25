23d07b7f10339a8f3bb93d87332d05e9adce1a4e
Gervig <gervig91@gmail.com>
2025-02-25 09:14:55 +0100

    init commit

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,21 @@
+package app;
+
+import app.config.HibernateConfig;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+        EntityManager em = emf.createEntityManager();
+
+
+
+        // Close the database connection:
+        em.close();
+        emf.close();
+    }
+
+}
---
+++ src/main/java/app/exceptions/ApiException.java
@@ -0,0 +1,21 @@
+package app.exceptions;
+
+public class ApiException extends RuntimeException
+{
+    private int code;
+    public ApiException(int code, String msg)
+    {
+        super((msg));
+        this.code = code;
+    }
+    public ApiException(int code, String msg, Exception e)
+{
+    super(msg, e);
+    this.code = code;
+}
+
+    public int getCode()
+    {
+        return this.code;
+    }
+}
