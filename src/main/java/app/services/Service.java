662e7a4cd1bf24fa5b28d7d28f4388982a18adeb
RikkeMariussen <cph-rm225@cphbusiness.dk>
2025-02-26 10:09:47 +0100

    Påbegyndt metode til at udfylde DTO

--- src/main/java/app/services/Service.java
+++ src/main/java/app/services/Service.java
@@ -1,8 +1,11 @@
 package app.services;
 
 import app.daos.impl.MovieDAO;
-import com.fasterxml.jackson.databind.JsonNode;
+import app.dtos.MovieDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -13,51 +16,71 @@ import java.util.List;
 
 public class Service
 {
+    //API_reader
     private final MovieDAO movieDAO;
     private final String API_KEY = System.getenv("api_key");
 
-    public Service(MovieDAO movieDAO)
-    {
+    //MovieService
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public Service(MovieDAO movieDAO){
         this.movieDAO = movieDAO;
     }
 
-    public List<Integer> getMovieApiIds()
-    {
-        HttpResponse<String> response;
-        ObjectMapper objectMapper = new ObjectMapper();
+    //Chrisser - Callable
+    //Casper - Alle ID'er fra api'et
+    //Rikke - Går alle id'erne og henter informationerne fra dem (for-loop)
 
-        String uri = "https://api.themoviedb.org/3/discover/" +
-                "movie?include_adult=false&include_video=false&language=da" +
-                "&release_date.gte=2020-02-26&release_date.lte=2025-02-26with_original_language=da&" + API_KEY;
+
+    public String getDataFromClientWithID(List<String> movieApiIds)
+    {
+        for (String movieApiId : movieApiIds)
+        {
+            String url = "https://api.themoviedb.org/3/movie/%%?append_to_response=credits&language=da&" + API_KEY;
+            String movieURL = url.replace("%%", movieApiId);
+            try
+            {
+                HttpClient client = HttpClient.newHttpClient();
+
+                HttpRequest request = HttpRequest.newBuilder()
+                        .version(HttpClient.Version.HTTP_1_1)
+                        .uri(new URI(movieURL))
+                        .GET()
+                        .build();
+
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                if (response.statusCode() == 200)
+                {
+                    String body = response.body();
+                    return body;
+                } else
+                {
+                    System.out.println("GET request failed. Status code: " + response.statusCode());
+                }
+
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    //MovieService
+    public MovieDTO convertFromJsonToDTO(String json)
+    {
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new JavaTimeModule());
 
         try
         {
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI(uri))
-                    .GET()
-                    .build();
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            MovieDTO movieDTO = objectMapper.readValue(
+                    objectMapper.readTree(json).get("movie_results").toString(),MovieDTO[].class)[0];
 
-            if (response.statusCode() == 200)
-            {
-                JsonNode rootNode = objectMapper.readTree(response.body());
-                List<Integer> IDs = new ArrayList<>();
-
-                // Iterate through the "results" array and extract "id"
-                for (JsonNode movieNode : rootNode.path("results"))
-                {
-                    IDs.add(movieNode.path("id").asInt());
-                }
-
-                return IDs;
-            } else
-            {
-                System.out.println("GET request failed. Status code: " + response.statusCode());
-            }
-        } catch (Exception e)
+            return movieDTO;
+        } catch (JsonProcessingException jPE)
         {
-            e.printStackTrace();
+            jPE.printStackTrace();
         }
         return null;
     }
