c47f51c9f189b4425aa4060bfea9e3f609ebb964
Gervig <gervig91@gmail.com>
2025-02-26 12:42:34 +0100

    Update Main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,16 @@
 package app;
 
 import app.config.HibernateConfig;
+import app.daos.impl.ActorDAO;
+import app.daos.impl.DirectorDAO;
+import app.daos.impl.GenreDAO;
+import app.daos.impl.MovieDAO;
+import app.entities.Director;
+import app.services.Service;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+
+import java.util.List;
 
 public class Main
 {
@@ -11,6 +19,16 @@ public class Main
         EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
         EntityManager em = emf.createEntityManager();
 
+        MovieDAO movieDAO = MovieDAO.getInstance(emf);
+        ActorDAO actorDAO = ActorDAO.getInstance(emf);
+        GenreDAO genreDAO = GenreDAO.getInstance(emf);
+        DirectorDAO director = DirectorDAO.getInstance(emf);
+
+        Service service = new Service(movieDAO);
+
+        List<String> movieApiIds = service.getMovieApiIds();
+
+        
 
         // Close the database connection:
         em.close();
