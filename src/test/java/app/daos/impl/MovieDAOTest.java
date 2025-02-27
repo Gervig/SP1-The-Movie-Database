7c8af1a0230d1045d187190d4b31d52cbcc243e2
Gervig <gervig91@gmail.com>
2025-02-27 10:25:40 +0100

    more setup

--- src/test/java/app/daos/impl/MovieDAOTest.java
+++ src/test/java/app/daos/impl/MovieDAOTest.java
@@ -1,10 +1,10 @@
 package app.daos.impl;
 
 import app.config.HibernateConfig;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class MovieDAOTest
 {
@@ -12,6 +12,17 @@ class MovieDAOTest
     private static final MovieDAO movieDAO = MovieDAO.getInstance(emf);
 
 
+    @BeforeEach
+    void setUp()
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+            em.createQuery("DELETE FROM Movie").executeUpdate();
+            em.createNativeQuery("ALTER SEQUENCE movie_id_seq RESTART WITH 1");
+
+        }
+    }
 
     @Test
     void getInstance()
